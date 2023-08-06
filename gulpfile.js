46c3bd9dce5bbe0b4a6ec621d140727992be1b02
Angel Kasabov <ankasabov142@gmail.com>
2023-08-06 20:26:21 +0300

    Updated functionality, SOC

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
-  return src("styles.scss").pipe(sass()).pipe(dest("css"));
+  return src("SASS_StyleSheets/**/*.scss").pipe(sass()).pipe(dest("css"));
 }
 
 function watchTask() {
-  watch(["styles.scss"], buildStyles);
+  watch(["SASS_StyleSheets/**/*.scss"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
