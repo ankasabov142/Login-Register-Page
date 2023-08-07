362ac03268c7ded00027f50b7fd2b7410df42af9
Angel Kasabov <ankasabov142@gmail.com>
2023-08-07 21:23:39 +0300

    Finished login/register panel swapping animation

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ $(function () {
   $("#btn").click(function (e) {
     let rippleEl = $(`<span class="btn-ripple"></span>`).appendTo(this);
     let pos = $(this).offset();
-    var width =100
-    var height = 100
+    var width = 100;
+    var height = 100;
 
-    var posX = e.pageX -510
+    var posX = e.pageX - 510;
     var posY = e.pageY - 220;
 
     var rippleStyle = new RippleStyle(width, height, posX, posY);
@@ -20,7 +20,7 @@ $(function () {
     rippleEl.css(rippleStyle);
   });
 
-  $("#.btn").on(
+  $("#btn").on(
     "animationed webkitAnimationEnd oanimationend MSAnimationEnd",
     ".btn-ripple",
     function () {
@@ -29,72 +29,104 @@ $(function () {
   );
 });
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDhZy4GB1EZua0_Ha2NIwNiHuuauLmrGNE",
+const btnReg = document.querySelector("#registerAnchor");
+const loginDiv = document.querySelector(".login");
+btnReg.addEventListener("click", (e) => {
+  loginDiv.classList.toggle("slideLeft");
+  // loginDiv.classList.remove("slideRight");
+  registerDiv.classList.remove("slideLeft");
+  setTimeout(() => {
+    registerDiv.classList.toggle("slideRight");
+  }, 2000);
+  //  registerDiv.classList.toggle("slideRight");
+});
 
-  authDomain: "login-register-page-4e082.firebaseapp.com",
+const btnLog = document.querySelector("#loginAnchor");
+const registerDiv = document.querySelector(".register");
+btnLog.addEventListener("click", (e) => {
+  registerDiv.classList.remove("slideRight");
 
-  projectId: "login-register-page-4e082",
+  registerDiv.classList.toggle("slideLeft");
 
-  storageBucket: "login-register-page-4e082.appspot.com",
+  setTimeout(() => {
+    loginDiv.classList.toggle("slideRight");
 
-  messagingSenderId: "450411434779",
+  },2000);
+  setTimeout(() => {
+    loginDiv.classList.remove("slideLeft");
+    loginDiv.classList.remove("slideRight");
+    registerDiv.classList.remove("slideLeft");
 
-  appId: "1:450411434779:web:4e2212ab2dfd8aeddef9be",
-};
+    registerDiv.classList.remove("slideRight");
+  }, 4000);
+  // loginDiv.classList.toggle("slideRight");
+  //  registerDiv.classList.toggle("slideRight");
+});
 
-// Initialize Firebase
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDhZy4GB1EZua0_Ha2NIwNiHuuauLmrGNE",
 
-const app = initializeApp(firebaseConfig);
+//   authDomain: "login-register-page-4e082.firebaseapp.com",
 
-//initialize vars
-const auth = firbase.auth();
-const database = firbase.database();
+//   projectId: "login-register-page-4e082",
 
-function register() {}
+//   storageBucket: "login-register-page-4e082.appspot.com",
 
-/* 
-VALIDATION
-if(validateEmail(email)==false|| validatePassword(password)==false){
-    alert('EMAIL OR PASS IS NOT OK')
-    return
-    //DONT CONTINUE RUNNING THE CODE
-}
+//   messagingSenderId: "450411434779",
 
-VALIDATE THE INPUTS THE SAME WAY!
+//   appId: "1:450411434779:web:4e2212ab2dfd8aeddef9be",
+// };
 
+// // Initialize Firebase
 
+// const app = initializeApp(firebaseConfig);
 
-*/
+// //initialize vars
+// const auth = firbase.auth();
+// const database = firbase.database();
 
-function validateEmail(email) {
-  const regex = /^[^@]+@\w+(\.\w+)+\w$/.test(str);
-  if (regex.test(email) == true) {
-    //Email is okay
-    return true;
-  } else {
-    //Email is not okay
-    return false;
-  }
-}
+// function register() {}
 
-function validatePassword(password) {
-  //Pass with lenght greater than 6 characters
-  if (password < 6) {
-    return false;
-  } else {
-    return true;
-  }
-}
+// /*
+// VALIDATION
+// if(validateEmail(email)==false|| validatePassword(password)==false){
+//     alert('EMAIL OR PASS IS NOT OK')
+//     return
+//     //DONT CONTINUE RUNNING THE CODE
+// }
 
-function validateField(field) {
-  if (field == null) {
-    return false;
-  }
+// VALIDATE THE INPUTS THE SAME WAY!
 
-  if (field.length <= 0) {
-    return false;
-  } else {
-    return true;
-  }
-}
+// */
+
+// function validateEmail(email) {
+//   const regex = /^[^@]+@\w+(\.\w+)+\w$/.test(str);
+//   if (regex.test(email) == true) {
+//     //Email is okay
+//     return true;
+//   } else {
+//     //Email is not okay
+//     return false;
+//   }
+// }
+
+// function validatePassword(password) {
+//   //Pass with lenght greater than 6 characters
+//   if (password < 6) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
+
+// function validateField(field) {
+//   if (field == null) {
+//     return false;
+//   }
+
+//   if (field.length <= 0) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
