e229b7cb0bca98c077bf2d6d7a0e1247c58d371d
Angel Kasabov <ankasabov142@gmail.com>
2023-08-07 21:52:57 +0300

    Added register functionality

--- app.js
+++ app.js
@@ -28,6 +28,36 @@ $(function () {
     }
   );
 });
+$(function () {
+  function RippleStyle(width, height, posX, posY) {
+    this.width = width <= height ? height : width;
+    this.height = width <= height ? height : width;
+    this.top = posY - this.height * 0.5;
+    this.left = posX - this.width * 0.5;
+  }
+
+  $(".registerButtonSubmit").click(function (e) {
+    let rippleEl = $(`<span class="btn-ripple"></span>`).appendTo(this);
+    let pos = $(this).offset();
+    var width = 100;
+    var height = 100;
+
+    var posX = e.pageX - 510;
+    var posY = e.pageY - 220;
+
+    var rippleStyle = new RippleStyle(width, height, posX, posY);
+
+    rippleEl.css(rippleStyle);
+  });
+
+  $(".registerButtonSubmit").on(
+    "animationed webkitAnimationEnd oanimationend MSAnimationEnd",
+    ".btn-ripple",
+    function () {
+      $(this).remove();
+    }
+  );
+});
 
 const btnReg = document.querySelector("#registerAnchor");
 const loginDiv = document.querySelector(".login");
@@ -50,8 +80,7 @@ btnLog.addEventListener("click", (e) => {
 
   setTimeout(() => {
     loginDiv.classList.toggle("slideRight");
-
-  },2000);
+  }, 2000);
   setTimeout(() => {
     loginDiv.classList.remove("slideLeft");
     loginDiv.classList.remove("slideRight");
